209fac451dcfc1f77c7267b1ef3fc7e88cc857e7
lsmith248 <64707626+lsmith248@users.noreply.github.com>
2020-09-02 15:29:02 -0400

    Add files via upload
    Robofriends App

---
+++ robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Anada Leah Patel',
+    username: 'Leah',
+    email: 'leahandjay@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Brent Jameson',
+    username: 'BJ',
+    email: 'BandL@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'CJ Rodriguez',
+    username: 'CJR',
+    email: 'Jrod@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Ramesh Raj Golopan',
+    username: 'Reddy Ray',
+    email: 'golopanraj@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Diamond Chennai Suarez-Gunzalez',
+    username: 'Chenni',
+    email: 'brightmorning@bms.com'
+  },
+  {
+    id: 6,
+    name: 'Landon Cruiz',
+    username: 'LCDolomyite',
+    email: 'landonc@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Darshawn',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Cheyenne DuBouis',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Jada Chantele Li-Chan',
+    username: 'Li.Chan',
+    email: 'blackgurlC@goblack.biz'
+  }
+];
